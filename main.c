b12fffe79a2ca2e9100d5870739064373a15c17c
scarfaced00 <jazmyne@naver.com>
2020-12-07 09:16:42 +0900

    ex2

--- main.c
+++ main.c
@@ -2,21 +2,30 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-void main()
+struct Book {
+	int number;
+	char title[10];
+};
+int main()
 {
-	char *pc=NULL;
-	int i=0;
+	struct Book *p;
 	
-	pc=(char*)malloc(100*sizeof(char));
-	if(pc==NULL){
-		printf("메모리 할당 오류\n");
-		exit(1);
+	p=(struct Book*)malloc(2*sizeof(struct Book));
+
+	if(p==NULL){
+		printf("memory allocation Error!\n");
+		return 1;
 	}
-	for(i=0;i<26;i++){
-		pc[i]='a'+i;
-	}
-	pc[i]=0;
-	printf("%s\n",pc);
-	free(pc);
+	
+	p->number=1;
+	strcpy(p->title,"C programming");
+	
+	(p+1)->number=2;
+	strcpy((p+1)->title,"Electronics");
+	
+	printf("%s %s\n",p->title,(p+1)->title);
+	
+	free(p);
+	return 0;
 }
 
